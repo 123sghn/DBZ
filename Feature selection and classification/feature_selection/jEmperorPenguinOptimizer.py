348d4da460af7ef5fce691b304724c11a77ce19b
SGHN <119744172+123sghn@users.noreply.github.com>
2024-04-07 18:13:46 +0800

    Update jEmperorPenguinOptimizer.py

--- Feature selection and classification/feature_selection/jEmperorPenguinOptimizer.py
+++ Feature selection and classification/feature_selection/jEmperorPenguinOptimizer.py
@@ -1,9 +1,20 @@
-#thres应该设置的尽量小一些才可能数组不为空，比如-1000
 import numpy as np
 from losses.jFitnessFunction import jFitnessFunction
 
+
 class jEmperorPenguinOptimizer:
-    def __init__(self, N, max_Iter, loss_func, alpha=0.9, beta=0.1, thres=0.5, tau=1, rho=0.2, eta=1):
+    def __init__(
+        self,
+        N,
+        max_Iter,
+        loss_func,
+        alpha=0.9,
+        beta=0.1,
+        thres=0.5,
+        tau=1,
+        rho=0.2,
+        eta=1
+    ):
         self.N = N
         self.max_Iter = max_Iter
         self.loss_func = loss_func
@@ -40,12 +51,16 @@ class jEmperorPenguinOptimizer:
         while t <= self.max_Iter:
             for i in range(N):
                 # Fitness
-                fit[i] = self.loss_func(x_train[:, X[i, :] > self.thres], x_test[:, X[i, :] > self.thres],
-                                        y_train, y_test)
+                fit[i] = self.loss_func(
+                    x_train[:, X[i, :] > self.thres],
+                    x_test[:, X[i, :] > self.thres],
+                    y_train,
+                    y_test,
+                )
                 # Best solution
                 if fit[i] < fitG:
                     fitG = fit[i]
-                    Xgb = X[i, :]
+                    Xgb = X[i, :].copy()
             # Generate radius in [0,1]
             R = np.random.rand()
             # Time (7)
@@ -54,7 +69,7 @@ class jEmperorPenguinOptimizer:
             else:
                 T0 = 1
             # Temperature profile (7)
-            T = T0 - (self.max_Iter / (t -1 - self.max_Iter))
+            T = T0 - (self.max_Iter / (t - 1 - self.max_Iter))
             for i in range(N):
                 for d in range(dim):
                     # Pgrid (10)
@@ -75,14 +90,14 @@ class jEmperorPenguinOptimizer:
                 XB[XB < lb] = lb
                 X[i, :] = XB
             curve[t - 1] = fitG
-            print('\nIteration %d Best (EPO)= %f' % (t, curve[t - 1]))
+            print("\nIteration %d Best (EPO)= %f" % (t, curve[t - 1]))
             t = t + 1
         # Select features
         Pos = np.arange(dim)
         Sf = Pos[(Xgb > self.thres) == 1]
         # Store results
         EPO = {}
-        EPO['sf'] = Sf
-        EPO['c'] = curve
+        EPO["sf"] = Sf
+        EPO["c"] = curve
 
         return EPO
