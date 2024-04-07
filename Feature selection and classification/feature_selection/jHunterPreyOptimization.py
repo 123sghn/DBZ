e02aa237b47689a2f139ac60438db1b0af61325a
SGHN <119744172+123sghn@users.noreply.github.com>
2024-04-07 18:19:24 +0800

    Update jHunterPreyOptimization.py

--- Feature selection and classification/feature_selection/jHunterPreyOptimization.py
+++ Feature selection and classification/feature_selection/jHunterPreyOptimization.py
@@ -1,9 +1,20 @@
-#thres应该设置的尽量小一些才可能数组不为空，比如-0.5
 import numpy as np
 from losses.jFitnessFunction import jFitnessFunction
 
+
 class jHunterPreyOptimization:
-    def __init__(self, N, max_Iter, loss_func, alpha=0.9, beta=0.1, thres=0.5, tau=1, rho=0.2, eta=1):
+    def __init__(
+        self,
+        N,
+        max_Iter,
+        loss_func,
+        alpha=0.9,
+        beta=0.1,
+        thres=0.5,
+        tau=1,
+        rho=0.2,
+        eta=1
+    ):
         self.N = N
         self.max_Iter = max_Iter
         self.loss_func = loss_func
@@ -16,11 +27,10 @@ class jHunterPreyOptimization:
 
     def optimize(self, x_train, x_test, y_train, y_test):
 
-        dim = x_train.shape[1]  # 维度
-        lb = 0  # 下界
-        ub = 1  # 上界
+        dim = x_train.shape[1]
+        lb = 0
+        ub = 1
         N = self.N
-        # N 需要大于30
         B = 0.7
 
         # HPO Parameters
@@ -30,11 +40,16 @@ class jHunterPreyOptimization:
         # Evaluate
         fitness = np.zeros(N)
         for i in range(N):
-            fitness[i] = self.loss_func(x_train[:,X[i, :] > self.thres], x_test[:,X[i, :] > self.thres], y_train, y_test)
+            fitness[i] = self.loss_func(
+                x_train[:, X[i, :] > self.thres],
+                x_test[:, X[i, :] > self.thres],
+                y_train,
+                y_test,
+            )
 
         indx = np.argmin(fitness)
         #
-        Xgb = X[indx, :]  # Xgb HPO
+        Xgb = X[indx, :].copy()  # Xgb HPO
         fitG = fitness[indx]
         curve[0] = fitG
 
@@ -46,35 +61,43 @@ class jHunterPreyOptimization:
                 r1 = np.random.rand(dim) < c
                 r2 = np.random.rand()
                 r3 = np.random.rand(dim)
-                idx = (r1 == 0)
+                idx = r1 == 0
                 z = r2 * idx + r3 * ~idx
                 if np.random.rand() < B:
                     xi = np.mean(X, axis=0)
                     dist = np.linalg.norm(xi - X, axis=1)
                     idxsortdist = np.argsort(dist)
                     SI = X[idxsortdist[kbest], :]
-                    X[i, :] = X[i, :] + 0.5 * ((2 * (c) * z * SI - X[i, :]) + (2 * (1 - c) * z * xi - X[i, :]))
+                    X[i, :] = X[i, :] + 0.5 * (
+                        (2 * (c) * z * SI - X[i, :]) + (2 * (1 - c) * z * xi - X[i, :])
+                    )
                 else:
                     for j in range(dim):
                         rr = -1 + 2 * z[j]
-                        X[i, j] = 2 * z[j] * np.cos(2 * np.pi * rr) * (Xgb[j] - X[i, j]) + Xgb[j]
+                        X[i, j] = (
+                            2 * z[j] * np.cos(2 * np.pi * rr) * (Xgb[j] - X[i, j])
+                            + Xgb[j]
+                        )
                 X[i, :] = np.clip(X[i, :], lb, ub)
                 # Evaluation
-                fitness[i] = self.loss_func(x_train[:,X[i, :] > self.thres], x_test[:,X[i, :] > self.thres], y_train, y_test)
+                fitness[i] = self.loss_func(
+                    x_train[:, X[i, :] > self.thres],
+                    x_test[:, X[i, :] > self.thres],
+                    y_train,
+                    y_test,
+                )
 
                 # Update Xgb
                 if fitness[i] < fitG:
-                    Xgb = X[i, :]
+                    Xgb = X[i, :].copy()
                     fitG = fitness[i]
             curve[t] = fitG
-            print('\nIteration %d Best (HPO)= %f' % (t, curve[t]))
+            print("\nIteration %d Best (HPO)= %f" % (t, curve[t]))
 
         Pos = np.arange(1, dim + 1)
         Sf = Pos[(Xgb > self.thres) == 1] - 1
 
-        # 主循环结束
         HPO = {}
-        HPO['sf'] = Sf  # best_X
-        HPO['c'] = curve
+        HPO["sf"] = Sf  # best_X
+        HPO["c"] = curve
         return HPO
-
